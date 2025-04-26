a63c6f18f7455e61d56cf93d17ca468af06b8b87
dhatguy <odunsiolakunbi@gmail.com>
2025-04-27 00:40:23 +0100

    chore: update RN

--- dist/bridgeUtils.d.ts
+++ dist/bridgeUtils.d.ts
@@ -5,5 +5,5 @@ export declare const SUBDEVLISTENER = "subDevListener";
 export declare const HOMESTATUS = "homeStatus";
 export declare const HOMECHANGE = "homeChange";
 export declare const SINGLETRANSFER = "SingleTransfer";
-export declare function addEvent(eventName: string, callback: (data: any) => any): import("react-native").EmitterSubscription;
+export declare function addEvent(eventName: string, callback: (data: any) => any): import("react-native/types").EmitterSubscription;
 export declare const bridge: (key: string, id: string | number) => string;
